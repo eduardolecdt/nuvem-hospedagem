c4862d033ab1faa729d2ffc796da45cc71484c1d
Eduardo Lecdt <eduardolecdt@gmail.com>
2021-09-18 10:12:41 -0300

    section Depoimento criada

--- assets/js/pages/registro-de-dominio/registro-de-dominio.js
+++ assets/js/pages/registro-de-dominio/registro-de-dominio.js
@@ -1,1 +1,31 @@
 // REGISTRO DE DOMÍNIO
+(function (){
+  /* ---- Constants ---- */
+
+  const URL = 'https://cliente.nuvemhospedagem.com.br/cart.php'
+  const PARAMETROS = '?a=add&domain=register&query='
+
+  /* ---- Elements ---- */
+
+  const sectionDominio = document.querySelector('section[name="apresentacao"]')
+  const inputDominio = sectionDominio.querySelector('input[name="dominio2"]')
+  const buttonAcessar = sectionDominio.querySelector('button[name="acessar2"]')
+
+  /* ---- Events ---- */
+
+  buttonAcessar.addEventListener('click', () => {
+    const dominio = inputDominio.value
+    const url = URL + PARAMETROS + dominio
+    navegar(url)
+  })
+
+  /* ---- Aux Functions ---- */
+
+  function navegar (url) {
+    const a = document.createElement('a')
+    a.href = url
+    a.target = '_blank'
+    a.click()
+  }
+  
+}())
