90fab8a665a1581c7f6a6c619073fc5d37041a3d
Densyy <densyy@hotmail.com>
2021-09-18 16:34:18 -0300

    wordpress planos - pt 2

--- assets/js/pages/hospedagem-wordpress/hospedagem-wordpress.js
+++ assets/js/pages/hospedagem-wordpress/hospedagem-wordpress.js
@@ -15,7 +15,7 @@
           semestral: 'R$ 16,90 = 20% de desconto',
           anual: 'R$ 15,90 = 35% de desconto'
         },
-        linkBotal: 'https://google.com/1'
+        linkBotao: 'https://google.com/1'
       },
       wordpressIntermediario: {
         processamentoIndice: 2,
@@ -30,7 +30,7 @@
           semestral: 'R$ 36,90 = 20% de desconto',
           anual: 'R$ 35,90 = 35% de desconto'
         },
-        linkBotal: 'https://google.com/1'
+        linkBotao: 'https://google.com/2'
       },
       wordpressAvancado: {
         processamentoIndice: 4,
@@ -45,7 +45,7 @@
           semestral: 'R$ 66,90 = 20% de desconto',
           anual: 'R$ 65,90 = 35% de desconto'
         },
-        linkBotal: 'https://google.com/1'
+        linkBotao: 'https://google.com/3'
       },
       wordpressEspecial: {
         processamentoIndice: 5,
@@ -60,7 +60,7 @@
           semestral: 'R$ 136,90 = 20% de desconto',
           anual: 'R$ 135,90 = 35% de desconto'
         },
-        linkBotal: 'https://google.com/1'
+        linkBotao: 'https://google.com/4'
       },
     }
   
@@ -72,12 +72,14 @@
     const buttonConsultarWordpressIntermediario = sectionPlanos.querySelector('button[name="consultarWordpressIntermediario"]')
     const buttonConsultarWordpressAvancado = sectionPlanos.querySelector('button[name="consultarWordpressAvancado"]')
     const buttonConsultarWordpressEspecial = sectionPlanos.querySelector('button[name="consultarWordpressEspecial"]')
+    const buttonAssinar = sectionPlanos.querySelector('button[name="assinar"]')
 
     const pMemoria = sectionPlanos.querySelector('p[name="memoria"]')
     const pEspaco = sectionPlanos.querySelector('p[name="espaco"]')
     const pTransferencia = sectionPlanos.querySelector('p[name="transferencia"]')
     const pContasEmail = sectionPlanos.querySelector('p[name="contasEmail"]')
     const pDominios = sectionPlanos.querySelector('p[name="dominios"]')
+    const divPotencia = sectionPlanos.querySelector('div[name="potencia"]')
 
     /* ---- Methods ---- */
 
@@ -86,52 +88,79 @@
       clickConsultarWordpressIntermediario()
       clickConsultarWordpressAvancado()
       clickConsultarWordpressEspecial()
+      clickAssinar()
     }
-    habilitarEventos()
     
     /* ---- Events ---- */
 
     function clickConsultarWordpressBasico () {
-      buttonConsultarWordpressBasico.addEventListener('click', () => {
-        const dados = STATE.wordpressBasico
-        limpar()
-        renderizar(dados)
-      })
+      buttonConsultarWordpressBasico.addEventListener('click', callbackClickConsultarWordpressBasico)
     }
 
     function clickConsultarWordpressIntermediario () {
-      buttonConsultarWordpressIntermediario.addEventListener('click', () => {
-        const dados = STATE.wordpressIntermediario
-        limpar()
-        renderizar(dados)
-      })
+      buttonConsultarWordpressIntermediario.addEventListener('click', callbackClickConsultarWordpressIntermediario)
     }
 
     function clickConsultarWordpressAvancado () {
-      buttonConsultarWordpressAvancado.addEventListener('click', () => {
-        const dados = STATE.wordpressAvancado
-        limpar()
-        renderizar(dados)
-      })
+      buttonConsultarWordpressAvancado.addEventListener('click', callbackClickConsultarWordpressAvancado)
     }
     
     function clickConsultarWordpressEspecial () {
-      buttonConsultarWordpressEspecial.addEventListener('click', () => {
-        const dados = STATE.wordpressEspecial
-        limpar()
-        renderizar(dados)
-      })
+      buttonConsultarWordpressEspecial.addEventListener('click', callbackClickConsultarWordpressEspecial)
+    }
+
+    function clickAssinar () {
+      buttonAssinar.addEventListener('click', callbackClickAssinar)
+    }
+
+    /* ---- Callbacks ---- */
+
+    function callbackClickConsultarWordpressBasico () {
+      const dados = STATE.wordpressBasico
+      limpar()
+
+      importarEstatisticas(dados)
+      ativarBotaoPlano(buttonConsultarWordpressBasico)
+      customizarLinkBotaoAssinar(dados.linkBotao)
+      selecionarPotenciaProcessamento(dados.processamentoIndice)
+    }
+
+    function callbackClickConsultarWordpressIntermediario () {
+      const dados = STATE.wordpressIntermediario
+      limpar()
+
+      importarEstatisticas(dados)
+      ativarBotaoPlano(buttonConsultarWordpressIntermediario)
+      customizarLinkBotaoAssinar(dados.linkBotao)
+      selecionarPotenciaProcessamento(dados.processamentoIndice)
+    }
+
+    function callbackClickConsultarWordpressAvancado () {
+      const dados = STATE.wordpressAvancado
+      limpar()
+
+      importarEstatisticas(dados)
+      ativarBotaoPlano(buttonConsultarWordpressAvancado)
+      customizarLinkBotaoAssinar(dados.linkBotao)
+      selecionarPotenciaProcessamento(dados.processamentoIndice)
+    }
+    
+    function callbackClickConsultarWordpressEspecial () {
+      const dados = STATE.wordpressEspecial
+      limpar()
+
+      importarEstatisticas(dados)
+      ativarBotaoPlano(buttonConsultarWordpressEspecial)
+      customizarLinkBotaoAssinar(dados.linkBotao)
+      selecionarPotenciaProcessamento(dados.processamentoIndice)
+    }
+
+    function callbackClickAssinar () {
+      const url = buttonAssinar.getAttribute('data-url')
+      window.location.assign(url)
     }
 
     /* ---- View ---- */
-
-    function renderizar (dados) {
-      pMemoria.innerText = dados.memoria
-      pEspaco.innerText = dados.espaco
-      pTransferencia.innerText = dados.transferencia
-      pContasEmail.innerText = dados.contasEmail
-      pDominios.innerText = dados.dominios
-    }
 
     function limpar () {
       pMemoria.innerText = ''
@@ -139,5 +168,52 @@
       pTransferencia.innerText = ''
       pContasEmail.innerText = ''
       pDominios.innerText = ''
+
+      desativarBotoesPlanos()
+      customizarLinkBotaoAssinar('')
+      limparPotenciaProcessamento()
     }
-  }())
+
+    function importarEstatisticas (dados) {
+      pMemoria.innerText = dados.memoria
+      pEspaco.innerText = dados.espaco
+      pTransferencia.innerText = dados.transferencia
+      pContasEmail.innerText = dados.contasEmail
+      pDominios.innerText = dados.dominios
+    }
+
+    function ativarBotaoPlano (button) {
+      desativarBotoesPlanos()
+      button.classList.add('ativo')
+    }
+
+    function desativarBotoesPlanos () {
+      buttonConsultarWordpressBasico.classList.remove('ativo')
+      buttonConsultarWordpressIntermediario.classList.remove('ativo')
+      buttonConsultarWordpressAvancado.classList.remove('ativo')
+      buttonConsultarWordpressEspecial.classList.remove('ativo')
+    }
+
+    function customizarLinkBotaoAssinar (url) {
+      buttonAssinar.setAttribute('data-url', url)
+    }
+
+    function selecionarPotenciaProcessamento (indice) {
+      limparPotenciaProcessamento()
+      const divs = divPotencia.querySelectorAll('div')
+      if (indice > divs.length) return
+      divs[indice - 1].classList.add('ativo')
+    }
+
+    function limparPotenciaProcessamento () {
+      const divs = divPotencia.querySelectorAll('div')
+      for (const div of divs) {
+        div.classList.remove('ativo')
+      }
+    }
+
+    /* ---- Start ---- */
+    
+    habilitarEventos()
+    callbackClickConsultarWordpressBasico()
+}())
