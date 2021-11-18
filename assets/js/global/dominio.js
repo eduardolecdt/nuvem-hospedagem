de49ecffac1ba1740de1c01f375addb41d4e90e4
Eduardo Lecdt <eduardolecdt@gmail.com>
2021-11-18 15:42:45 -0300

    Ajustes NAVBAR + Global

--- assets/js/global/dominio.js
+++ assets/js/global/dominio.js
@@ -23,8 +23,16 @@
   function navegar (url) {
     const a = document.createElement('a')
     a.href = url
-    a.target = '_blank'
+    a.target = '_self'
     a.click()
   }
+
+  inputDominio.addEventListener('keyup', function(e){
+    if (e.keyCode == 13) {
+      const dominio = inputDominio.value
+      const url = URL + PARAMETROS + dominio
+      navegar(url)
+    }
+  })
   
 }())
